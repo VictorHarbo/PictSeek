d096ded306c651b9a3652005dba4065c4915e5fa
Victor Harbo Johnston <vholesen@hotmail.com>
2024-06-08 15:53:48 +0200

    update downloader script

--- src/main/resources/startICloudIngest.sh
+++ src/main/resources/startICloudIngest.sh
@@ -1,12 +1,12 @@
 CONF_PATH=/home/admin/conf/PictSeek-devel.yaml
 
 # Extract the username
-username=$(yq '.icloudDownloader.username' $CONF_PATH)
+username=$(yq -r '.icloudDownloader.user' $CONF_PATH)
 # Extract the password
-password=$(yq '.icloudDownloader.password' $CONF_PATH)
-downloadDir=$(yq '.icloudDownloader.downloadDirectory' $CONF_PATH)
+password=$(yq -r '.icloudDownloader.password' $CONF_PATH)
+downloadDir=$(yq  -r '.icloudDownloader.downloadDirectory' $CONF_PATH)
 
 
+echo "Starting download for user: " $username
 
-
-docker run -i -v "$downloadDir":/downloadedImages icloudpd/icloudpd:latest icloudpd --directory /downloadedImages --username "$username" --password $password --recent 10 --skip-videos --folder-structure none
+docker run -i -v "$downloadDir:/downloadedImages" icloudpd/icloudpd:latest icloudpd --directory /downloadedImages --username $username --password $password --recent 10 --skip-videos --folder-structure none
