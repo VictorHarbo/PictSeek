0eff50c60d02d565dc7fc814ff9fc6249cd7e10e
Victor Harbo Johnston <vholesen@hotmail.com>
2024-06-12 23:06:42 +0200

    Add boilerplate for a react frontend

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to PictSeek Archive
+        </p>
+          A frontend application should be available here at some point.
+      </header>
+    </div>
+  );
+}
+
+export default App;
